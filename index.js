51f3f5b5390458c0a7d88e954308b72cd3d995a3
Matheus Felipe Akira <matheusfelipeakira@gmail.com>
2024-07-10 21:39:58 -0300

    Commit 2 - Replace Temp with Query
    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const { readFileSync } = require('fs');
 
 function gerarFaturaStr(fatura, pecas) {
 
+    // Função query
+    function getPeca(apresentacao) {
+        return pecas[apresentacao.id];
+    }
+
     // Função extraída
-    function calcularTotalApresentacao(apre, peca) {
+    function calcularTotalApresentacao(apre) {
         let total = 0;
+        const peca = getPeca(apre);
         switch (peca.tipo) {
             case "tragedia":
                 total = 40000;
@@ -32,16 +38,15 @@ function gerarFaturaStr(fatura, pecas) {
         { style: "currency", currency: "BRL", minimumFractionDigits: 2 }).format;
 
     for (let apre of fatura.apresentacoes) {
-        const peca = pecas[apre.id];
-        let total = calcularTotalApresentacao(apre, peca);
+        let total = calcularTotalApresentacao(apre);
 
         // Créditos para próximas contratações
         creditos += Math.max(apre.audiencia - 30, 0);
-        if (peca.tipo === "comedia")
+        if (getPeca(apre).tipo === "comedia")
             creditos += Math.floor(apre.audiencia / 5);
 
         // Mais uma linha da fatura
-        faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+        faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
         totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
